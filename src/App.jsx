bf9c99bff8da36c2462fbe0f1481ecb290fe51f3
Bhavyajprajapati <157686673+Bhavyajprajapati@users.noreply.github.com>
2025-09-21 13:31:32 +0530

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import CameraQualityTester from "./CameraQualityTester";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <CameraQualityTester />
